a997145ac90dd6d166286dff7cc4a6adda15f117
unknown <hado.roman.ihorovych@gmail.com>
2021-08-31 16:29:22 +0300

    Updates

---
+++ precache-manifest.4ed86b79e873387f1f5cd34178992003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f589979c5afe6208f1df3b783d3bb91",
+    "url": "./index.html"
+  },
+  {
+    "revision": "48b748efb1ed9e76c7b9",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b748efb1ed9e76c7b9",
+    "url": "./static/js/main.652e826a.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
